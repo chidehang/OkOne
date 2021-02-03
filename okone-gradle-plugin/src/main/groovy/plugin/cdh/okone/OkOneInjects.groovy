452b17433d68693ddc674a2a6d1f856eb76365e2
cdh <cdh@meitu.com>
2021-02-03 16:47:27 +0800

    [feature] 集成ASM

--- okone-gradle-plugin/src/main/groovy/plugin/cdh/okone/OkOneInjects.groovy
+++ okone-gradle-plugin/src/main/groovy/plugin/cdh/okone/OkOneInjects.groovy
@@ -3,6 +3,7 @@ package plugin.cdh.okone
 import javassist.ClassPool
 import org.apache.commons.io.FileUtils
 import plugin.cdh.okone.inject.AsyncCallInjector
+import plugin.cdh.okone.inject.BaseClassInjector
 import plugin.cdh.okone.inject.ClientBuilderInjector
 import plugin.cdh.okone.inject.DispatcherInjector
 import plugin.cdh.okone.inject.IClassInjector
@@ -20,11 +21,11 @@ class OkOneInjects {
         tmpDir.mkdirs()
 
         // 添加代码注入处理器
-        List<IClassInjector> injectorList = new LinkedList<>()
-        injectorList.add(new ClientBuilderInjector())
-        injectorList.add(new RequestInjector())
-        injectorList.add(new AsyncCallInjector())
-        injectorList.add(new DispatcherInjector())
+        List<BaseClassInjector> injectorList = new LinkedList<>()
+//        injectorList.add(new ClientBuilderInjector())
+//        injectorList.add(new RequestInjector())
+//        injectorList.add(new AsyncCallInjector())
+//        injectorList.add(new DispatcherInjector())
 
         // 创建新的目标产物jar文件
         destFile.createNewFile()
@@ -36,13 +37,17 @@ class OkOneInjects {
         while (iterator.hasNext()) {
             JarEntry entry = iterator.next()
             // 查找对应的代码注入处理器
-            IClassInjector injector = matchInjector(injectorList, entry.name)
+            BaseClassInjector injector = matchInjector(injectorList, entry.name)
             if (injector != null) {
                 println("匹配Injector: " + entry.name + " => " + injector)
-                // 注入代码，并返回修改后的文件
-                File injectedFile = injector.inject(tmpDir, pool)
-                println("拷贝注入修改后的class文件: " + injectedFile + " ==> " + injectedFile.exists() + ", " + entry.name)
-                writeFile(jos, injectedFile, entry.name)
+                InputStream oldInput = new BufferedInputStream(jarFile.getInputStream(entry))
+                // 注入代码，并返回修改后的内容
+                byte[] injectedBytes = injector.inject(oldInput)
+                // 写入注入修改后的内容
+                jos.putNextEntry(new JarEntry(entry.name))
+                jos.write(injectedBytes)
+                jos.closeEntry()
+                println("${entry.name}->写入成功")
             } else {
                 // 原样写入
                 jos.putNextEntry(new JarEntry(entry.name))
@@ -62,26 +67,15 @@ class OkOneInjects {
         FileUtils.deleteDirectory(tmpDir)
     }
 
-    private static IClassInjector matchInjector(List<IClassInjector> injectorList, String name) {
-        Iterator<IClassInjector> i = injectorList.iterator();
+    private static BaseClassInjector matchInjector(List<BaseClassInjector> injectorList, String name) {
+        Iterator<BaseClassInjector> i = injectorList.iterator()
         while (i.hasNext()) {
-            IClassInjector injector = i.next();
+            BaseClassInjector injector = i.next();
             if (injector.handles(name)) {
                 i.remove()
                 return injector
             }
         }
         return null
-    }
-
-    private static void writeFile(JarOutputStream jos, File file, String entryName) {
-        jos.putNextEntry(new JarEntry(entryName))
-        FileInputStream fis = new FileInputStream(file)
-        byte[] buffer = new byte[1024 * 8]
-        int length = -1;
-        while ((length = fis.read(buffer)) != -1) {
-            jos.write(buffer, 0, length)
-        }
-        fis.close()
     }
 }
